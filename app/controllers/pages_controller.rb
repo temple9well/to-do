856a750cce5c89425450a052956756dafb962aa0
yauyuirate <temple9well@gmail.com>
2017-02-13 20:30:54 +0900

    Finished ui ux touches

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,10 +1,13 @@
 class PagesController < ApplicationController
   def home
+    if current_user
+  		redirect_to tasks_path
+  	end
   end
 
   def about
   end
 
-  def test 
+  def test
   end
 end
